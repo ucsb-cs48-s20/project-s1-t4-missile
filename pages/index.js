040e2d498ea167545528e410fa32a07b6b0f8549
Alex Lancaster <aaa633@ucsb.edu>
2020-04-26 21:17:32 -0700

    AL- centered game and title text; gray background color

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ import { Component } from 'react'
 import io from 'socket.io-client'
 import { render } from 'react-dom'
 
+const styles = {
+    body: {
+        backgroundColor: '#888888',
+    },
+
+    container: {
+        textAlign: 'center',
+    },
+}
+
 class Test extends Component {
     constructor(props) {
         super(props)
@@ -12,22 +22,39 @@ class Test extends Component {
     }
 
     componentDidMount() {
-        /*this.socket = io();
+        /*
+        this.socket = io();
         this.socket.on('now', data => {
             this.setState({
                 hello: data.message
             })
-        });*/
+        });
+        */
+
+        //set body style
         document.title = this.state.pageTitle;
+        var oldBodyStyle = document.body.style;
+
+        for (var i in styles.body) {
+            if (styles.body.hasOwnProperty(i)) {           
+                document.body.style[i] = styles.body[i];
+            }
+        }
+    }
+
+    componentWillUnmount() {
+        //reset body style
+        document.body.style = oldBodyStyle;
     }
 
     render() {
         return (
-            <div>
+            <div style={styles.container}>
                 <h1>{this.state.pageTitle}</h1>
                 <script src="/socket.io/socket.io.js"></script>
                 <script src="//cdn.jsdelivr.net/npm/phaser@3.22.0/dist/phaser.js"></script>
                 <script type='module' src='/static/game.js'></script>
+                <div id="gameWindow"></div>
             </div>
         )
     }
--- public/static/game.js
+++ public/static/game.js
@@ -6,6 +6,7 @@ let endScene = new EndScene();
 
 let config = {
     type: Phaser.AUTO, 
+    parent: 'gameWindow',
     width: 1280, 
     height: 720,
     physics: {
