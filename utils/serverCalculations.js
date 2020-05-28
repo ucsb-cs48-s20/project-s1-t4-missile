2c59698979aeca7778c861d5632a35db24f8c2a6
Brian Qiu <brianlqiu5@gmail.com>
2020-05-28 14:00:37 -0700

    bq - added unit tests for gameCalculations

--- utils/serverCalculations.js
+++ utils/serverCalculations.js
@@ -1,3 +1,3 @@
-const distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1), 2);
+export const distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1), 2);
 
 module.exports = { distance }
