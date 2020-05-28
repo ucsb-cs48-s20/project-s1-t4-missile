63e2662899afe8182188f4029e2fb7daf8694175
Brian Qiu <brianlqiu5@gmail.com>
2020-05-28 14:14:26 -0700

    bq - removed export from serverCalculations

--- utils/serverCalculations.js
+++ utils/serverCalculations.js
@@ -2,7 +2,7 @@ function throwError(message) {
     throw new Error(message);
 }
 
-export const distance = (x1, y1, x2, y2) => {
+const distance = (x1, y1, x2, y2) => {
     typeof x1 === 'number' && typeof y1 === 'number' && typeof x2 === 'number' && typeof y2 === 'number' || throwError('all arguments must be numbers');
     return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
 }
