84c1f88edcd8200bedbea28fe8765bf5fb55d8f6
Brian Qiu <brianlqiu5@gmail.com>
2020-05-28 14:07:52 -0700

    bq - added testing for serverCalculations

--- utils/serverCalculations.js
+++ utils/serverCalculations.js
@@ -1,3 +1,9 @@
-export const distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1), 2);
+function throwError(message) {
+    throw new Error(message);
+}
 
+export const distance = (x1, y1, x2, y2) => {
+    typeof x1 === 'number' && typeof y1 === 'number' && typeof x2 === 'number' && typeof y2 === 'number' || throwError('all arguments must be numbers');
+    return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
+}
 module.exports = { distance }
