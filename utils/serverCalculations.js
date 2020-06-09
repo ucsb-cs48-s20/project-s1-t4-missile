1f89ec3d69ec91eda33861e85bca05345e259027
Brian Qiu <brianlqiu5@gmail.com>
2020-06-08 18:53:11 -0700

    bq - server.js refactoring & documentation

--- utils/serverCalculations.js
+++ utils/serverCalculations.js
@@ -1,5 +1,5 @@
 /* Calculates the distance between (x1,y1) and (x2,y2) */
-export const distance = (x1, y1, x2, y2) => {
+const distance = (x1, y1, x2, y2) => {
     let invalid = '';
     if (typeof x1 != 'number') { invalid += ' x1 is not a number\n' }
     if (typeof y1 != 'number') { invalid += ' y1 is not a number\n' }
@@ -8,3 +8,5 @@ export const distance = (x1, y1, x2, y2) => {
     if (invalid != '') { throw new Error(`All arguments must be numbers.\n${invalid}`); }
     return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
 }
+
+module.exports = { distance };
