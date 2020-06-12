7c10e9f89f5cbcf75447d7d56de1bd506b28bcf4
Brian Qiu <brianlqiu5@gmail.com>
2020-06-11 17:39:28 -0700

    bq/hz - further adjusting of game volume

--- public/static/gameScene.js
+++ public/static/gameScene.js
@@ -195,7 +195,7 @@ class GameScene extends Phaser.Scene {
         });
 
         /* Load Music */
-        this.music = this.sound.add('bgm', { loop: true, volume: 0.4 });
+        this.music = this.sound.add('bgm', { loop: true, volume: 0.2 });
         this.music.play();
 
         /* Creates object groups */
